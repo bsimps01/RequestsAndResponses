07e7b2b8feef4524f9e436521db53524ec936f49
Ben Simpson <b.albertsimpson@gmail.com>
2020-01-23 14:40:42 -0800

    Updates

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('<int: question_id>/', views.detail, name='detail'),
+    path('<int: question_id>/results/', views.results, name='results'),
+    path('<int: question_id>/vote/', views.vote, name='vote'),
+]
+
