7041d04f1c23c99fb7f78f114287c8b85ff74920
Ben Simpson <b.albertsimpson@gmail.com>
2020-01-30 14:37:56 -0800

    updates to models and documentation

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('<int: question_id>/', views.detail, name='detail'),
+    path('specifics<int: question_id>/', views.detail, name='detail'),
     path('<int: question_id>/results/', views.results, name='results'),
     path('<int: question_id>/vote/', views.vote, name='vote'),
 ]
